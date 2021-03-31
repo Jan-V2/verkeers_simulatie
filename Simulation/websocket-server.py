0012848f17c8f93a2cec15fde44c41aef6a7033d
AvianaLilian <maxime.hendriks@student.nhlstenden.com>
2021-03-31 22:09:55 +0200

    Update websocket

--- Simulation/websocket-server.py
+++ Simulation/websocket-server.py
@@ -1,16 +1,59 @@
 import asyncio
 import websockets
+import json
+import logging
 
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print(f"<{name}")
+logging.basicConfig()
 
-    greeting = f"Hello {name}!"
+STATE = {"value": 0}
 
-    await websocket.send(greeting)
+USERS = set()
+
+def state_event():
+    #Dumps is used to write a Python object into a JSON string
+    return json.dumps({"type": "state", **STATE})
+
+def users_event():
+    return json.dumps({"type": "users", "count": len(USERS)})
+
+async def notify_state():
+    if USERS: #asyncio.wait doesn't accept an empty list
+        message = state_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+async def notify_users():
+    if USERS:
+        message = users_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+async def register(websocket):
+    USERS.add(websocket)
+    await notify_users()
+
+async def unregister(websocket):
+    USERS.remove(websocket)
+    await notify_users()
+
+async def counter(websocket, path):
+    #register(websocket) sends user_event() to websocket
+    await register(websocket)
+    try:
+        await websocket.send(state_event())
+        async for message in websocket:
+            data = json.loads(message)
+            #if data["action"] == "minus":
+                #STATE["value"] -= 1
+                #await notify_state()
+            #elif data["action"] == "plus":
+                #STATE["value"] += 1
+                #await notify_state()
+            #else:
+                #logging.error("unsupported event: {} ", data)
+    finally:
+        await unregister(websocket)
 
 #wrapper around the even loop's create_server() method. It creates and starts a Server, then it wraps the server in a websocketserver and returns the websocketserver
-start_server = websockets.serve(hello, port=6969)
+start_server = websockets.serve(counter, port=6969)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
