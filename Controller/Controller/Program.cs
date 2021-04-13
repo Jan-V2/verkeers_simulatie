81d16122cef982b44b6c58ab68eaae4ad21c2559
BuildTools <unconfigured@null.spigotmc.org>
2021-04-13 12:28:54 +0200

    Simulation now sends useable traffic lights. The controller saves these and sends messages to the simulation changing it's state. If the simulation has any change in active sensors it will notify the controller which will use that information to prioritize the traffic lights.

--- Controller/Controller/Program.cs
+++ Controller/Controller/Program.cs
@@ -14,6 +14,16 @@ namespace Controller
 {
     class Program
     {
+        static int message_id = 0;
+        static List<TrafficLight> trafficLights = new List<TrafficLight>();
+        static List<Sensor> sensors = new List<Sensor>();
+        static List<int> currentWorkingLights = new List<int>();
+        static long cycle_state_time = DateTimeOffset.Now.ToUnixTimeSeconds();
+        static string cycle_state = "red";
+        static bool cycle_state_fresh = true;
+
+        static bool debug = false;
+
         static async Task Main(string[] args)
         {
             string ip = "ws://127.0.0.1:6969";
@@ -23,22 +33,125 @@ namespace Controller
                     try
                     {
                         await socket.ConnectAsync(new Uri(ip), CancellationToken.None);
-                        Console.WriteLine("connected");
+                        Console.WriteLine("Connected");
                         //await Send(socket, "data");
                         Receive(socket);
+                        Thread.Sleep(500);
                         Stopwatch sw = new Stopwatch();
-                        bool tlcolor = true;
+                        //bool tlcolor = true;
                         while (true)
                         {
-                            sw.Start(); //Voer dit uit aan het begin van de loop
+                            //Voer dit uit aan het begin van de loop
+                            sw.Start();
 
-                            await Send(socket, "{\"msg_id\": 0, \"msg_type\": \"perform_change_state\", \"data\": [{\"id\": 0, \"state\":" + (tlcolor ? "\"green\"" : "\"red\"") + "}]}");
-                            tlcolor = !tlcolor;
+                            {
+                                /*while (true)
+                                {
+                                    message_id++;
+                                    await Send(socket, "{\"msg_id\": " + message_id.ToString() + ", \"msg_type\": \"test\", \"data\": [1, 2]}");
+                                    Console.WriteLine(socket.State.ToString());
+                                    Thread.Sleep(2000);
+                                }*/
+                                string jsonstring;
+                                switch (cycle_state)
+                                {
+                                    case "red":
+                                        cycle_state = "green";
+                                        currentWorkingLights.Clear();
+                                        cycle_state_time = CurrentTime();
+                                        cycle_state_fresh = true;
+                                        /*message_id++;
+                                        jsonstring = "{\"msg_id\": " + message_id + ", \"msg_type\": \"notify_traffic_light_state\", \"data\": [";
+                                        for (int c = 0; c < trafficLights.Count; c++)
+                                        {
+                                            jsonstring = jsonstring + "{\"id\": " + trafficLights[c].id + ", \"state\": \"red\"}";
+                                            if ((c + 1) != trafficLights.Count)
+                                                jsonstring = jsonstring + ", ";
+                                        }
+                                        jsonstring = jsonstring + "]}";
+                                        await Send(socket, jsonstring);*/
+                                        break;
+                                    case "orange":
+                                        double longest_clearing_time = 0;
+                                        foreach (int i in currentWorkingLights)
+                                            if (TrafficLightByID(i).clearing_time > longest_clearing_time)
+                                                longest_clearing_time = TrafficLightByID(i).clearing_time;
+
+                                        if (cycle_state_fresh)
+                                        {
+                                            message_id++;
+                                            jsonstring = "{\"msg_id\": " + message_id + ", \"msg_type\": \"notify_traffic_light_change\", \"data\": [";
+                                            for (int c = 0; c < trafficLights.Count; c++)
+                                            {
+                                                jsonstring = jsonstring + "{\"id\": " + trafficLights[c].id + ", \"state\": " + (currentWorkingLights.Contains(trafficLights[c].id) ? "\"orange\"" : "\"red\"") + "}";
+                                                if ((c + 1) != trafficLights.Count)
+                                                    jsonstring = jsonstring + ", ";
+                                            }
+                                            jsonstring = jsonstring + "]}";
+                                            await Send(socket, jsonstring);
+                                            cycle_state_fresh = false;
+                                        }else if((CurrentTime() - cycle_state_time) > longest_clearing_time)
+                                        {
+                                            cycle_state = "red";
+                                            cycle_state_fresh = true;
+                                            cycle_state_time = CurrentTime();
+                                        }
+                                        break;
+                                    case "green":
+                                        if(cycle_state_fresh)
+                                        {
+                                            int oldestid = 1;
+                                            long longesttimesinceupdate = 0;
+                                            foreach (TrafficLight tl in trafficLights)
+                                                if ((CurrentTime() - tl.lastupdated) >= longesttimesinceupdate)
+                                                {
+                                                    oldestid = tl.id;
+                                                    longesttimesinceupdate = (CurrentTime() - tl.lastupdated);
+                                                }
+                                            if(debug)
+                                                Console.WriteLine("Longest waiting time: " + longesttimesinceupdate);
+
+                                            currentWorkingLights.Add(oldestid);
+
+                                            foreach (Sensor sensor in sensors)
+                                                if (sensor.isOn)
+                                                    if (!currentWorkingLights.Contains(sensor.id) && evaluate_trafficlight_compatibility(sensor.id, currentWorkingLights))
+                                                        currentWorkingLights.Add(sensor.id);
+
+                                            foreach (TrafficLight tl in trafficLights)
+                                                if (!currentWorkingLights.Contains(tl.id) && evaluate_trafficlight_compatibility(tl.id, currentWorkingLights))
+                                                    currentWorkingLights.Add(tl.id);
+
+                                            foreach (TrafficLight tl in trafficLights)
+                                                if (currentWorkingLights.Contains(tl.id))
+                                                    tl.Touch();
+                                            cycle_state_fresh = false;
+                                            message_id++;
+                                            jsonstring = "{\"msg_id\": " + message_id + ", \"msg_type\": \"notify_traffic_light_change\", \"data\": [";
+                                            for (int c = 0; c < trafficLights.Count; c++)
+                                            {
+                                                jsonstring = jsonstring + "{\"id\": " + trafficLights[c].id + ", \"state\": " + (currentWorkingLights.Contains(trafficLights[c].id) ? "\"green\"" : "\"red\"") + "}";
+                                                if ((c + 1) != trafficLights.Count)
+                                                    jsonstring = jsonstring + ", ";
+                                            }
+                                            jsonstring = jsonstring + "]}";
+                                            await Send(socket, jsonstring);
+                                        }
+                                        else if((CurrentTime() - cycle_state_time) >  10)
+                                        {
+                                            cycle_state = "orange";
+                                            cycle_state_fresh = true;
+                                            cycle_state_time = CurrentTime();
+                                        }
+                                        break;
+                                }
+                            }
 
                             //Voer dit uit aan het einde van de loop
                             sw.Stop();
                             if (sw.ElapsedMilliseconds < 1000)
                                 Thread.Sleep(1000 - (int)sw.ElapsedMilliseconds);
+                            //Console.WriteLine("Current cycle state: " + cycle_state + ", fresh: " + cycle_state_fresh + ", duration of state right now: " + (CurrentTime() - cycle_state_time) + " seconds.");
                         }
                     }
                     catch (Exception ex)
@@ -48,12 +161,19 @@ namespace Controller
                         string input = Console.ReadLine();
                         if (input != "")
                             ip = "ws://" + input + ":6969";
+                        CleanUp();
                     }
             } while (true);
         }
 
-        static async Task Send(ClientWebSocket socket, string data) =>
+        static async Task Send(ClientWebSocket socket, string data)
+        {
             await socket.SendAsync(Encoding.UTF8.GetBytes(data), WebSocketMessageType.Text, true, CancellationToken.None);
+            //Console.WriteLine(data);
+            //Console.WriteLine(socket.State.ToString());
+            var details = JObject.Parse(data);
+            Console.WriteLine("<- | MSG #" + details["msg_id"] + ", Type: " + details["msg_type"]);
+        }
 
         static async Task Receive(ClientWebSocket socket)
         {
@@ -68,7 +188,6 @@ namespace Controller
                         result = await socket.ReceiveAsync(buffer, CancellationToken.None);
                         ms.Write(buffer.Array, buffer.Offset, result.Count);
                     } while (!result.EndOfMessage);
-
                     if (result.MessageType == WebSocketMessageType.Close)
                         break;
 
@@ -76,19 +195,125 @@ namespace Controller
                     using (var reader = new StreamReader(ms, Encoding.UTF8))
                     {
                         string jsonstring = await reader.ReadToEndAsync();
+                        
                         var details = JObject.Parse(jsonstring);
 
-                        Console.Write("MSG #" + details["msg_id"].ToString() + " - ");
+                        message_id = details["msg_id"].ToObject<int>();
+                        
+                        Console.WriteLine("-> | MSG #" + message_id + ", Type: " + details["msg_type"]);
 
-                        if (details["msg_type"].ToString() == "notify_state_change")
-                        {
-                            Console.WriteLine("Received traffic light #" + details["data"][0]["id"].ToString() + " with clearing time of " + details["data"][0]["clearing_time"] + ".");
-
+                        switch (details["msg_type"].ToString()){
+                            case "initialization":
+                                trafficLights.Clear();
+                                foreach (var tl in details["data"])
+                                {
+                                    int a = tl["id"].ToObject<int>();
+                                    List<int> b = tl["crosses"].ToObject<List<int>>();
+                                    double c = tl["clearing_time"].ToObject<double>();
+                                    trafficLights.Add(new TrafficLight(a, b, c));
+                                }
+                                break;
+                            case "notify_sensor_change":
+                                foreach(var sensor in details["data"])
+                                {
+                                    if (!SensorExists(sensor["id"].ToObject<int>()))
+                                        sensors.Add(new Sensor(sensor["id"].ToObject<int>()));
+                                    ChangeSensor(sensor["id"].ToObject<int>(), sensor["vehicles_coming"].ToObject<bool>() || sensor["vehicles_waiting"].ToObject<bool>());
+                                }
+                                break;
                         }
-
                     }
                 }
             } while (true);
+        }
+
+        static bool SensorExists(int sensorid)
+        {
+            foreach (Sensor sensor in sensors)
+                if (sensor.id == sensorid)
+                    return true;
+
+            return false;
+        }
+        
+        static void ChangeSensor(int sensorid, bool state)
+        {
+            foreach (Sensor sensor in sensors)
+                if (sensor.id == sensorid)
+                    sensor.isOn = state;
+        }
+
+        static long CurrentTime()
+        {
+            return DateTimeOffset.Now.ToUnixTimeSeconds();
+        }
+
+        static TrafficLight TrafficLightByID(int id)
+        {
+            for(int c = 0; c < trafficLights.Count; c++)
+            {
+                if(trafficLights[c].id == id)
+                {
+                    return trafficLights[c];
+                }
+            }
+            return new TrafficLight(746, new List<int>(), 746.0F);
+        }
+
+        static bool evaluate_trafficlight_compatibility(int newtl, List<int> currentlist)
+        {
+            foreach(int valID in currentlist)
+            {
+                if (TrafficLightByID(valID).crosses.Contains(newtl))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void CleanUp()
+        {
+            message_id = 0;
+            trafficLights.Clear();
+            sensors.Clear();
+            currentWorkingLights.Clear();
+            cycle_state = "red";
+            cycle_state_fresh = true;
+        }
+    }
+
+    class TrafficLight
+    {
+        public int id;
+        public List<int> crosses;
+        public double clearing_time;
+        public string state;
+        public long lastupdated;
+        public TrafficLight(int id, List<int> crosses, double clearing_time)
+        {
+            this.id = id;
+            this.crosses = crosses;
+            this.clearing_time = clearing_time;
+            lastupdated = DateTimeOffset.Now.ToUnixTimeSeconds();
+            state = "red";
+        }
+
+        public void Touch()
+        {
+            lastupdated = DateTimeOffset.Now.ToUnixTimeSeconds();
+        }
+    }
+    
+    class Sensor
+    {
+        public int id;
+        public bool isOn;
+
+        public Sensor(int id)
+        {
+            this.id = id;
+            this.isOn = false;
         }
     }
 }
--- Simulation/websocket-server.py
+++ Simulation/websocket-server.py
@@ -2,6 +2,7 @@ import asyncio
 import websockets
 import json
 import logging
+import time
 
 logging.basicConfig()
 
@@ -11,17 +12,78 @@ USERS = set()
 
 message_id = 0
 
-def state_event():
-    #Dumps is used to write a Python object into a JSON string
-    return json.dumps({"type": "state", **STATE})
+messages = []
 
-def users_event():
-    return json.dumps({"msg_id": new_messageid(), "msg_type": "users", "count": len(USERS)})
+traffic_lights = []
 
-async def notify_state():
-    if USERS: #asyncio.wait doesn't accept an empty list
-        message = state_event()
-        await asyncio.wait([user.send(message) for user in USERS])
+sensors = []
+
+oldsensors = []
+
+debug = False
+
+async def main_loop(websocket):
+    global oldsensors
+    while True:
+        await asyncio.sleep(1)
+        #await websocket.send("{\"msg_type\": \"test\", \"msg_id\": 5")
+        #message = '{{"msg_type": "test", "msg_id": {0}}}'.format(new_messageid())
+        #print(message)
+        #await Send(websocket, message)
+        for sensor in sensors:
+            if sensor.isOn and (traffic_light_by_id(sensor.id).state == "green"):
+                sensor.isOn = False
+        if (not [oldsensors[i].isOn for i in range(0, len(oldsensors))] == [sensors[i].isOn for i in range(0, len(sensors))]):
+            message = {
+                "msg_id": new_messageid(),
+                "msg_type": "notify_sensor_change",
+                "data": [{"id": sensor.id, "vehicles_waiting": sensor.isOn, "vehicles_coming": False, "emergency_vehicle": False} for sensor in sensors]
+            }
+
+            oldsensors.clear()
+            for sensor in sensors:
+                newsensor = Sensor(sensor.id)
+                newsensor.isOn = sensor.isOn
+                oldsensors.append(newsensor)
+
+            await Send(websocket, json.dumps(message))
+
+        if debug:
+            print([sensor.isOn for sensor in sensors])
+
+async def Receive(websocket):
+    global message_id
+    while True:
+        try:
+            async for jsonmessage in websocket:
+                #print(jsonmessage)
+                message = json.loads(jsonmessage)
+                messages.append(message)
+                message_id = message["msg_id"]
+                print("-> | MSG #{0}, Type: {1}".format(message_id, message["msg_type"]))
+                if message["msg_type"] == "notify_traffic_light_change":
+                    for tl in message["data"]:
+                        change_traffic_light_state(tl["id"], tl["state"])
+        except Exception as ex:
+            print("Something went wrong in receiving and/or decoding a message.")
+            print(type(ex).__name__, ex.args)
+            await asyncio.sleep(1)
+
+async def Send(websocket, message):
+    jsonmessage = json.loads(message)
+    print("<- | MSG #{0}, Type: {1}".format(jsonmessage["msg_id"], jsonmessage["msg_type"]))
+    await websocket.send(message)
+
+def change_traffic_light_state(id, state):
+    for i in range(0, len(traffic_lights)):
+        if traffic_lights[i].id == id:
+            traffic_lights[i].state = state
+
+def traffic_light_by_id(id):
+    for tl in traffic_lights:
+        if tl.id == id:
+            return tl
+    return TrafficLight(746, [], 746.0)
 
 def new_messageid():
     global message_id
@@ -29,50 +91,66 @@ def new_messageid():
     return message_id
 
 def initialization():
+    traffic_lights.clear()
+    crossingsstringlist = ["000000000000000000001111000100010000","000000000000000000000001000100010000","000000000000000000001110000000000000","000000000000000000001110000000000000","000000000000000000000001000100010000","000000000000000000000011101000100000","000000000000000000000010001000100000","000000000000000000000001100000000000","000000000000000000000001100000000000","000000000000000000000010001000100000","000000000000000000000100101111000000","000000000000000000000100100001000000","000000000000000000000000001110000000","000000000000000000000000001110000000","000000000000000000000100100001000000","000000000000000000001001000011110000","000000000000000000001001000010000000","000000000000000000000000000001110000","000000000000000000000000000001110000","000000000000000000001001000010000000","101100000000000110010001001111100000","101100000011001000000001101001100000","101101100100000000000000011000100000","110011011000000110011100011111110000","000001011011001000000100010001000000","000000000000000000000011101000100000","000001100110110000001111010001100000","110010000010110000001001000001110000","000000000010110110011001000000000000","000000000011001101101101101100000000","000001100100000101101111011100000000","110010000000000101100001000100000000","000000000000000000000000000000000110","000000000000000000000000000000001011","000000000000000000000000000000001101","000000000000000000000000000000000110"]
+    for tlnr in range(0,36):
+        crosses = []
+        for x in range(0, len(crossingsstringlist)):
+            if crossingsstringlist[tlnr][x] == '1':
+                crosses.append(x + 1)
+        traffic_lights.append(TrafficLight(tlnr + 1, crosses, 4.0))
+    for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33]:
+        sensors.append(Sensor(i))
+    if debug:
+        for sensor in sensors:
+            sensor.isOn = True
     message = {
         "msg_id": new_messageid(),
-        "msg_type": "notify_state_change",
-        "data":[
-            {
-                "id": 5,
-                "crosses": [1, 2],
-                "clearing_time": 3.0
-            }
-        ]
+        "msg_type": "initialization",
+        "data": [{"id": tl.id, "crosses": tl.crosses, "clearing_time": tl.clearing_time} for tl in traffic_lights]
     }
     return json.dumps(message)
 
-
-async def notify_users():
-    if USERS:
-        message = users_event()
-        await asyncio.wait([user.send(message) for user in USERS])
-
 async def register(websocket):
     USERS.add(websocket)
-    #await notify_users()
+    print("Client with ip: {0}:{1}".format(websocket.remote_address[0], websocket.remote_address[1]), "joined.")
 
 async def unregister(websocket):
     USERS.remove(websocket)
-    await notify_users()
 
 async def counter(websocket, path):
-    #register(websocket) sends user_event() to websocket
+    #starttime = time.time()
     await register(websocket)
+    #while True:
+    #    await asyncio.sleep(1)
+    #    if not websocket.open:
+    #        print(time.time() - starttime)
+    await Send(websocket, initialization())
+    #await Receive(websocket)
+
     try:
-        #Ontvangen
-        await websocket.send(initialization())
-        #await websocket.send(state_event())
-        while True:
-            message = await websocket.recv()
-            decoded_message = json.loads(message)
-            message = decoded_message
-            print("MSG #{}".format(message["msg_id"]), "- Changed traffic light #{} to".format(message["data"][0]["id"]), message["data"][0]["state"])
+        await asyncio.gather(Receive(websocket), main_loop(websocket))
+    except Exception as ex:
+        print("Something went wrong while sending the initial message or in the main loop, someone likely disconnected.")
+        print(type(ex).__name__, ex.args)
+        await asyncio.sleep(1)
     finally:
         await unregister(websocket)
 
+class TrafficLight:
+    def __init__(self, id, crosses, clearing_time):
+        self.id = id
+        self.crosses = crosses
+        self.clearing_time = clearing_time
+        self.state = "red"
+
+class Sensor:
+    def __init__(self, id):
+        self.id = id
+        self.isOn = False
+
 #wrapper around the even loop's create_server() method. It creates and starts a Server, then it wraps the server in a websocketserver and returns the websocketserver
-start_server = websockets.serve(counter, port=6969)
+start_server = websockets.serve(counter, port=6969, ping_interval=None, ping_timeout=None)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
