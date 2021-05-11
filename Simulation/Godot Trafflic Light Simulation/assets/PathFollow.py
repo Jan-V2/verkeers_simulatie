4aada3dd678654c2a90b577ebcf46f2e84329d82
johnvanderholt <jdqwertqwert@gmail.com>
2021-05-11 12:13:23 +0200

    basic path toegevoegd

--- Simulation/Godot Trafflic Light Simulation/assets/PathFollow.py
+++ Simulation/Godot Trafflic Light Simulation/assets/PathFollow.py
@@ -8,9 +8,17 @@ class PathFollow(PathFollow):
 	# member variables here, example:
 	a = export(int, default=10)
 
-
 	def _ready(self):
-		pass
+		self.path_idx = 0;
+		self.paths =["kruis", "uit"]
 		
 	def _process(self, delta):
-		self.set_offset(self.get_offset() + self.a * delta)
+		progress = self.get_unit_offset()
+		if progress >= 1:
+			self.set_unit_offset(0)
+			new_parent = self.get_parent().get_parent().get_node(self.paths[self.path_idx])
+			self.get_parent().remove_child(self)
+			new_parent.add_child(self)
+			self.path_idx += 1
+		else:
+			self.set_offset(self.get_offset() + self.a * delta)
