ab45893e17ec2975eaa7fa0d5e72e468d2281e34
AvianaLilian <maxime.hendriks@student.nhlstenden.com>
2021-05-21 12:09:20 +0200

    Added cars stopping to all intersection traffic lights

--- Simulation/Godot Trafflic Light Simulation/scripts/TrafficLightArea.py
+++ Simulation/Godot Trafflic Light Simulation/scripts/TrafficLightArea.py
@@ -13,34 +13,157 @@ class TrafficLightArea(Node):
 		pass
 	
 	def _on_TrafficLightArea21_body_entered(self, body):
-		#print('hoi')
 		vehicles_waiting21 = []
+		body.getparent() = car
 		greenlight = self.find_parent("World").find_node("trafficlight21a").get_node("GreenLight").visible
 		
 		while greenlight != True:
-			body.get_parent().speed = 0
-			vehicles_waiting21.extend(body.get_parent())
+			car.speed = 0
+			vehicles_waiting21.extend(car)
 		else:
 			for vehicle in vehicles_waiting21:
 				vehicle.speed = 10
 				vehicles_waiting21.clear()
 	
-	#def _on_TrafficLightArea23_body_entered(self, body):
+	def _on_TrafficLightArea22_body_entered(self, body):
+		vehicles_waiting22 = []
+		body.getparent() = car
+		greenlight = self.find_parent("World").find_node("trafficlight22a").get_node("GreenLight").visible
+		
+		while greenlight != True:
+			car.speed = 0
+			vehicles_waiting22.extend(car)
+		else:
+			for vehicle in vehicles_waiting22:
+				vehicle.speed = 10
+				vehicles_waiting22.clear()
 	
-	#def _on_TrafficLightArea24_body_entered(self, body):
+	def _on_TrafficLightArea23_body_entered(self, body):
+		vehicles_waiting23 = []
+		body.getparent() = car
+		greenlight = self.find_parent("World").find_node("trafficlight23a").get_node("GreenLight").visible
+		
+		while greenlight != True:
+			car.speed = 0
+			vehicles_waiting23.extend(car)
+		else:
+			for vehicle in vehicles_waiting23:
+				vehicle.speed = 10
+				vehicles_waiting23.clear()
 	
-	#def _on_TrafficLightArea25_body_entered(self, body):
+	def _on_TrafficLightArea24_body_entered(self, body):
+		vehicles_waiting24 = []
+		body.getparent() = car
+		greenlight = self.find_parent("World").find_node("trafficlight24a").get_node("GreenLight").visible
+		
+		while greenlight != True:
+			car.speed = 0
+			vehicles_waiting24.extend(car)
+		else:
+			for vehicle in vehicles_waiting24:
+				vehicle.speed = 10
+				vehicles_waiting24.clear()
 	
-	#def _on_TrafficLightArea26_body_entered(self, body):
+	def _on_TrafficLightArea25_body_entered(self, body):
+		vehicles_waiting25 = []
+		body.getparent() = car
+		greenlight = self.find_parent("World").find_node("trafficlight25a").get_node("GreenLight").visible
+		
+		while greenlight != True:
+			car.speed = 0
+			vehicles_waiting25.extend(car)
+		else:
+			for vehicle in vehicles_waiting25:
+				vehicle.speed = 10
+				vehicles_waiting25.clear()
+				
+	def _on_TrafficLightArea26_body_entered(self, body):
+		vehicles_waiting26 = []
+		body.getparent() = car
+		greenlight = self.find_parent("World").find_node("trafficlight26a").get_node("GreenLight").visible
+		
+		while greenlight != True:
+			car.speed = 0
+			vehicles_waiting26.extend(car)
+		else:
+			for vehicle in vehicles_waiting26:
+				vehicle.speed = 10
+				vehicles_waiting26.clear()
 	
-	#def _on_TrafficLightArea27_body_entered(self, body):
+	def _on_TrafficLightArea27_body_entered(self, body):
+		vehicles_waiting27 = []
+		body.getparent() = car
+		greenlight = self.find_parent("World").find_node("trafficlight27a").get_node("GreenLight").visible
+		
+		while greenlight != True:
+			car.speed = 0
+			vehicles_waiting27.extend(car)
+		else:
+			for vehicle in vehicles_waiting27:
+				vehicle.speed = 10
+				vehicles_waiting27.clear()
 	
-	#def _on_TrafficLightArea28_body_entered(self, body):
+	def _on_TrafficLightArea28_body_entered(self, body):
+		vehicles_waiting28 = []
+		body.getparent() = car
+		greenlight = self.find_parent("World").find_node("trafficlight28a").get_node("GreenLight").visible
+		
+		while greenlight != True:
+			car.speed = 0
+			vehicles_waiting28.extend(car)
+		else:
+			for vehicle in vehicles_waiting28:
+				vehicle.speed = 10
+				vehicles_waiting28.clear()
 	
-	#def _on_TrafficLightArea29_body_entered(self, body):
+	def _on_TrafficLightArea29_body_entered(self, body):
+		vehicles_waiting29 = []
+		body.getparent() = car
+		greenlight = self.find_parent("World").find_node("trafficlight29a").get_node("GreenLight").visible
+		
+		while greenlight != True:
+			car.speed = 0
+			vehicles_waiting29.extend(car)
+		else:
+			for vehicle in vehicles_waiting29:
+				vehicle.speed = 10
+				vehicles_waiting29.clear()
 	
-	#def _on_TrafficLightArea30_body_entered(self, body):
+	def _on_TrafficLightArea30_body_entered(self, body):
+		vehicles_waiting30 = []
+		body.getparent() = car
+		greenlight = self.find_parent("World").find_node("trafficlight30").get_node("GreenLight").visible
+		
+		while greenlight != True:
+			car.speed = 0
+			vehicles_waiting30.extend(car)
+		else:
+			for vehicle in vehicles_waiting30:
+				vehicle.speed = 10
+				vehicles_waiting30.clear()
 	
-	#def _on_TrafficLightArea31_body_entered(self, body):
+	def _on_TrafficLightArea31_body_entered(self, body):
+		vehicles_waiting31 = []
+		body.getparent() = car
+		greenlight = self.find_parent("World").find_node("trafficlight31").get_node("GreenLight").visible
+		
+		while greenlight != True:
+			car.speed = 0
+			vehicles_waiting31.extend(car)
+		else:
+			for vehicle in vehicles_waiting31:
+				vehicle.speed = 10
+				vehicles_waiting31.clear()
 	
-	#def _on_TrafficLightArea32_body_entered(self, body):
+	def _on_TrafficLightArea32_body_entered(self, body):
+		vehicles_waiting32 = []
+		body.getparent() = car
+		greenlight = self.find_parent("World").find_node("trafficlight32").get_node("GreenLight").visible
+		
+		while greenlight != True:
+			car.speed = 0
+			vehicles_waiting32.extend(car)
+		else:
+			for vehicle in vehicles_waiting32:
+				vehicle.speed = 10
+				vehicles_waiting32.clear()
