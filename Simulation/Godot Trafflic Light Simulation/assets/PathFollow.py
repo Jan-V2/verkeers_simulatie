899777caef1062158742bac9fa2b36696e8957b5
johnvanderholt <jdqwertqwert@gmail.com>
2021-05-21 11:03:53 +0200

    paths toegevoegd

--- Simulation/Godot Trafflic Light Simulation/assets/PathFollow.py
+++ Simulation/Godot Trafflic Light Simulation/assets/PathFollow.py
@@ -18,20 +18,12 @@ class PathFollow(PathFollow):
 		
 	def _process(self, delta):
 		if self.enabled:
-			if len(self.paths) < 1:
-				self.paths = [self.path_0, self.path_1]
+
 			self.visible = True
 			progress = self.get_unit_offset()
 			if progress >= 1:
-				if self.path_idx >= len(self.paths):
 					self.queue_free()
 					self.enabled = False
-				else:
-					self.set_unit_offset(0)
-					new_parent = self.get_parent().get_parent().get_node(self.paths[self.path_idx])
-					self.get_parent().remove_child(self)
-					new_parent.add_child(self)
-					self.path_idx += 1
 			else:
 				self.set_offset(self.get_offset() + self.speed * delta)
 	
--- Simulation/Godot Trafflic Light Simulation/path.py
+++ Simulation/Godot Trafflic Light Simulation/path.py
@@ -13,7 +13,7 @@ class path(Path):
 		self.time_passed = 0
 		self.new_truck = ResourceLoader.load("res://assets/path_truck.tscn")
 		self.paths =[['8,15|8,19', ['N->E', '22,25|15,25']],['9,15|9,19', ['N->S1', '8,31|8,26']],['10,15|10,19', ['N->S2', '9,31|9,26']],['11,15|11,19', ['N->E', '22,25|15,25']],['22,20|15,20', ['E->N1', '14,15|14,19']],['22,21|15,21', ['E->N2', '13,15|13,19']],['0,23|7,23', ['W->N', '13,15|13,19']],['0,24|7,24', ['W->E', '22,25|15,25']],['0,25|7,25', ['W->S', '8,31|8,26']],['11,31|11,26', ['S->W',  '0,21|7,21']],['12,31|12,26', ['S->N1', '13,15|13,19']],['13,31|13,26', ['S->N2', '14,15|14,19']],['14,31|14,26', ['S->E', '22,25|15,25']],]
-		
+		self.paths2 =['N->E','N->S1','N->S2','N->E','E->N1','E->N2','E->W','E->S','W->N','W->E','W->S','S->W','S->N1','S->N2','S->E',]
 		
 	
 		
@@ -22,10 +22,10 @@ class path(Path):
 		if self.time_passed > 0.5:
 			self.time_passed = 0
 			truck = self.new_truck.instance()
-			path = choice(self.paths)
-			self.get_parent().get_node(path[0]).add_child(truck)
-			truck.path_0 = path[1][0]
-			truck.path_1 = path[1][1]
+			path = choice(self.paths2)
+			self.get_parent().get_node(path).add_child(truck)
+			#truck.path_0 = path[1][0]
+			#truck.path_1 = path[1][1]
 
 			truck.enabled = True
 	
