cd49df5d856936b6231d47ccaefb42e816c45dd6
johnvanderholt <jdqwertqwert@gmail.com>
2021-05-22 12:17:44 +0200

    merge gedebugd

--- Simulation/Godot Trafflic Light Simulation/path.py
+++ Simulation/Godot Trafflic Light Simulation/path.py
@@ -4,37 +4,30 @@ from random import choice
 
 @exposed
 class path(Path):
-
-	# member variables here, example:
-	a = export(int)
-	b = export(str, default='foo')
-
 	def _ready(self):
 		self.time_passed = 0
-		self.new_bus = ResourceLoader.load("res://assets/path_van.tscn"))
-		self.paths =['N->W','N->E','N->S1','N->S2','N->E','E->N1','E->N2','E->W','E->S','W->N','W->E','W->S','S->W','S->N1','S->N2','S->E',]
+		self.new_bus = ResourceLoader.load("res://assets/path_truck.tscn")
+		self.new_truck = ResourceLoader.load("res://assets/path_truck.tscn")
+		self.paths =['N->W','N->E','N->S1','N->S2','N->E','E->N1','E->N2','E->W','E->S', "E->E",'W->N','W->E','W->S','S->W','S->N1','S->N2','S->E',]
 		self.path_idx = 0
 
 
 	def _process(self, delta):
 		self.time_passed += delta
-		if self.time_passed > 2:
+		if self.time_passed > 0.5:
 			self.time_passed = 0
-			path = choice(self.paths2)
+			path = self.paths[self.path_idx]
 			if path == 'E->E':
 				car = self.new_bus.instance()
-				rotation = car.get_rotation()
-				rotation.y += (90 * 0.0174533)
-				car.set_rotation(rotation)
 				self.get_parent().get_node(path).add_child(car)
 				car.enabled = True
 			else:
 				truck = self.new_truck.instance()
 				self.get_parent().get_node(self.paths[self.path_idx]).add_child(truck)
-                self.path_idx += 1
-                if not self.path_idx < len(self.paths):
-                	self.path_idx = 0
 				truck.enabled = True
+		self.path_idx += 1
+		if not self.path_idx < len(self.paths):
+					self.path_idx = 0
 
 	def _on_Area_body_entered(one, two):
 		print("entered area")
