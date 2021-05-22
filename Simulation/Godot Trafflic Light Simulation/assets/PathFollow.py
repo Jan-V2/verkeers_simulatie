b09e55e57e8714a9857f97946b1afe5cfb98373a
johnvanderholt <jdqwertqwert@gmail.com>
2021-05-22 12:04:38 +0200

    autos worden met de juiste hoek gespawnt

--- Simulation/Godot Trafflic Light Simulation/assets/PathFollow.py
+++ Simulation/Godot Trafflic Light Simulation/assets/PathFollow.py
@@ -1,6 +1,6 @@
 from godot import exposed, export
 from godot import *
-
+from math import cos
 
 @exposed
 class PathFollow(PathFollow):
@@ -16,6 +16,26 @@ class PathFollow(PathFollow):
 		#self.paths =["cross", "out"]
 		self.paths =[]
 		self.visible = False
+		point = self.get_parent().get_curve().interpolate_baked(0)
+		point2 = self.get_parent().get_curve().interpolate_baked(0.001)
+		this_dir = (point2 - point).normalized()
+		ref1 = Vector3(0.04706317558884621, -3.458734454397927e-06, -0.9988918900489807)
+		ref2 = Vector3(-0.013520398177206516, -3.4035724638670217e-06, -0.9999085664749146)
+		rad_to_deg = lambda r: (r / 3.1416) * 180
+		
+		ref_diff = rad_to_deg(ref1.angle_to(ref2))
+		
+		rad_ref1 = this_dir.angle_to(ref1)
+		rad_ref2 = this_dir.angle_to(ref2)
+		angle = rad_to_deg(rad_ref1)
+		if rad_ref1 >= rad_ref2:
+			self.set_rotation_degrees(Vector3(0,angle, 0))
+		else:
+			if ref_diff < angle < 180 - ref_diff :
+				self.set_rotation_degrees(Vector3(0,angle+180, 0))
+			else:
+				self.set_rotation_degrees(Vector3(0,angle, 0))
+
 		
 	def _process(self, delta):
 		if self.enabled:
@@ -30,6 +50,7 @@ class PathFollow(PathFollow):
 	
 	def _on_Area_body_entered(self, body):
 		self.speed = 0
+		
 		
 	def _on_Area_body_exited(self, body):
 		self.speed = 10
--- Simulation/Godot Trafflic Light Simulation/path.py
+++ Simulation/Godot Trafflic Light Simulation/path.py
@@ -12,18 +12,23 @@ class path(Path):
 	def _ready(self):
 		self.time_passed = 0
 		self.new_truck = ResourceLoader.load("res://assets/path_truck.tscn")
-		self.paths =[['8,15|8,19', ['N->E', '22,25|15,25']],['9,15|9,19', ['N->S1', '8,31|8,26']],['10,15|10,19', ['N->S2', '9,31|9,26']],['11,15|11,19', ['N->E', '22,25|15,25']],['22,20|15,20', ['E->N1', '14,15|14,19']],['22,21|15,21', ['E->N2', '13,15|13,19']],['0,23|7,23', ['W->N', '13,15|13,19']],['0,24|7,24', ['W->E', '22,25|15,25']],['0,25|7,25', ['W->S', '8,31|8,26']],['11,31|11,26', ['S->W',  '0,21|7,21']],['12,31|12,26', ['S->N1', '13,15|13,19']],['13,31|13,26', ['S->N2', '14,15|14,19']],['14,31|14,26', ['S->E', '22,25|15,25']],]
-		self.paths2 =['N->E','N->S1','N->S2','N->E','E->N1','E->N2','E->W','E->S','W->N','W->E','W->S','S->W','S->N1','S->N2','S->E',]
-		
+		self.paths =['N->W','N->E','N->S1','N->S2','N->E','E->N1','E->N2','E->W','E->S','W->N','W->E','W->S','S->W','S->N1','S->N2','S->E',]
+		self.path_idx = 0
 	
 		
 	def _process(self, delta):
 		self.time_passed += delta
-		if self.time_passed > 0.5:
+		if self.time_passed > 1:
 			self.time_passed = 0
 			truck = self.new_truck.instance()
-			path = choice(self.paths2)
-			self.get_parent().get_node(path).add_child(truck)
+			#path = choice(self.paths2)
+			print(self.paths[self.path_idx])
+			self.get_parent().get_node(self.paths[self.path_idx]).add_child(truck)
+			
+			self.path_idx += 1
+			if not self.path_idx < len(self.paths):
+				self.path_idx = 0
+			
 			#truck.path_0 = path[1][0]
 			#truck.path_1 = path[1][1]
 
